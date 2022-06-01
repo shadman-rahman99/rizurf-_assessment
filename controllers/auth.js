2ddd979b4c9513adbca5b791bc874118a36043f6
Jamee Shahriyar <shahriyar@graduate.utm.my>
2022-06-01 16:04:45 +0600

    alpha2 version 1.0

---
+++ controllers/auth.js
@@ -0,0 +1,11 @@
+exports.register = (req, res, next) => {
+  res.send("Register Route");
+};
+
+exports.login = (req, res, next) => {
+  res.send("Login Route");
+};
+
+exports.forgotpassword = (req, res, next) => {
+  res.send("Forgot Password ROute");
+};
